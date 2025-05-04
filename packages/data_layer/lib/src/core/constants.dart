13ba29b52ebdee440c5386c619742dbe3050ef5e
NextTopCodeChallenge <shahabgolpaiegan2@gmail.com>
2025-05-05 01:13:29 +0330

    WIP: news list repository impl getTopHeadlinesUS no connection add no already saved test case

--- packages/data_layer/lib/src/core/constants.dart
+++ packages/data_layer/lib/src/core/constants.dart
@@ -1,3 +1,4 @@
 const int pageSize = 25;
 const String offlineFirstListHasAlreadyEndedErrorMessage =
     "The List has already ended!\n To get the latest news, please check your internet connection";
+const notConnectedToTheInternetError = "Your are not connected to the internet.";
